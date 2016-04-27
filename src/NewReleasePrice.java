8aba696553f5de4666be36231ad15be350593559
LeifBernsdorf <leif.bernsdorf@tariwe.de>
2016-04-27 14:17:29 +0200

    STEP: Created abstract price class and implementations

---
+++ src/NewReleasePrice.java
@@ -0,0 +1,14 @@
+public class NewReleasePrice extends Price {
+
+    @Override
+    public double getCharge(int days) {
+        return days * 3;
+    }
+
+    @Override
+    public int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+
+
+}
---
+++ src/Price.java
@@ -0,0 +1,6 @@
+public abstract class Price {
+
+    public abstract double getCharge(int days);
+    public abstract int getPriceCode();
+
+}
