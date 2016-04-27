3bd71833feaab1b84ce49fbb88a63892cf48c849
LeifBernsdorf <leif.bernsdorf@tariwe.de>
2016-04-27 13:14:55 +0200

    adding linebreaks, removing semicolons, deleting junk

--- src/Customer.java
+++ src/Customer.java
@@ -3,21 +3,26 @@ import java.lang.*;
 import java.util.*;
 
 class Customer {
+
     private String name;
     private Vector rentals = new Vector();
+
     public Customer (String newname){
         name = newname;
-    };
+    }
+
     public void addRental(Rental arg) {
         rentals.addElement(arg);
-    };
+    }
+
     public String getName (){
         return name;
-    };
+    }
+
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
+        Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
@@ -35,6 +40,7 @@ class Customer {
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
+
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
